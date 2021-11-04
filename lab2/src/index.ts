130e5b177c2570b5d1b310611b2b0f2dcf9a0035
Siarhei Kharlanau1 <siarhei_kharlanau1@epam.com>
2021-11-04 13:55:51 +0300

    feat: implement interface for tasks

--- lab2/src/index.ts
+++ lab2/src/index.ts
@@ -1,37 +1,80 @@
 import {searching} from './algorithms/searching'
-import {fillArray, List} from "./utils";
-import {BinarySearchTree} from "./algorithms/tree/binaryTree";
+import {fillArray, getK, List} from "./utils";
+import {BinarySearchTree} from "./algorithms/binaryTree";
 import {HashTable} from "./algorithms/hash/hashTable";
+import {mergeHybridSort} from "./algorithms/sorting/mergeHybridSort";
 
-// const findElement = () => {
-//   const container: HTMLElement = document.querySelector('#search')
-//   const length: number = parseInt((container.querySelector('.length') as HTMLInputElement).value, 10)
-//   const maxEl: number = parseInt((container.querySelector('.max') as HTMLInputElement).value, 10)
-//   const find: number = parseInt((container.querySelector('.find') as HTMLInputElement).value, 10)
-//
-//   const arr: number[] = fillArray(length, maxEl)
-//   arr.sort((a: number, b: number) => a - b)
-//   console.log(arr)
-//
-//   console.log(searching(arr, find, 'binary'))
-//   console.log(searching(arr, find, 'interpolation'))
-// }
-//
-// const findElementBtn: HTMLButtonElement = document.querySelector('#search .submit')
-// findElementBtn.addEventListener('click', () => {
-//   findElement()
-// })
+const findElement = () => {
+  const container: HTMLElement = document.querySelector('#search')
+  const length: number = parseInt((container.querySelector('.length') as HTMLInputElement).value, 10)
+  const maxEl: number = parseInt((container.querySelector('.max') as HTMLInputElement).value, 10)
+  const find: number = parseInt((container.querySelector('.find') as HTMLInputElement).value, 10)
 
-// const tree = BinarySearchTree.construct<number>([4, 2, 6, 0, 1, 3, 9, 8, 10, 5, 12, 13])
-// console.log(tree.isBalanced(tree.rootTree))
-// tree.balance()
-// console.log(tree.isBalanced(tree.rootTree))
-// console.log(tree.rootTree)
+  const arr: number[] = mergeHybridSort(fillArray(length, maxEl), getK(length))
 
-const hashTable = new HashTable(10, 'chaining')
+  console.log(searching(arr, find, 'binary'))
+  console.log(searching(arr, find, 'interpolation'))
+}
+const findElementBtn: HTMLButtonElement = document.querySelector('#search .submit')
+findElementBtn.addEventListener('click', () => {
+  findElement()
+})
+
+const tree: BinarySearchTree<number> = BinarySearchTree.construct(fillArray(6, 20))
+const addToTreeBtn: HTMLButtonElement = document.querySelector('#tree .add')
+addToTreeBtn.addEventListener('click', () => {
+  const container: HTMLElement = document.querySelector('#tree')
+  const elToAdd: number = parseInt((container.querySelector('.el') as HTMLInputElement).value, 10)
+
+  tree.insert(elToAdd)
+
+  let traverse: number[] = []
+  tree.inOrderTraverse(tree.rootTree, (el) => {
+    traverse.push(el.data)
+  })
+  console.log('inorder', traverse)
+  traverse = []
+  tree.preOrderTraverse(tree.rootTree, (data) => {
+    traverse.push(data)
+  })
+  console.log('preorder', traverse)
+  traverse = []
+  tree.postOrderTraverse(tree.rootTree, (data) => {
+    traverse.push(data)
+  })
+  console.log('postorder', traverse)
+})
+const findTreeElBtn: HTMLButtonElement = document.querySelector('#tree .find')
+findTreeElBtn.addEventListener('click', () => {
+  const container: HTMLElement = document.querySelector('#tree')
+  const k: number = parseInt((container.querySelector('.min') as HTMLInputElement).value, 10)
+  console.log('kth min el', tree.kthSmallest(tree.rootTree, k))
+})
+const balanceTreeBtn: HTMLButtonElement = document.querySelector('#tree .balance')
+balanceTreeBtn.addEventListener('click', () => {
+  tree.balance()
+
+  let traverse: number[] = []
+  tree.inOrderTraverse(tree.rootTree, (el) => {
+    traverse.push(el.data)
+  })
+  console.log('inorder', traverse)
+  traverse = []
+  tree.preOrderTraverse(tree.rootTree, (data) => {
+    traverse.push(data)
+  })
+  console.log('preorder', traverse)
+  traverse = []
+  tree.postOrderTraverse(tree.rootTree, (data) => {
+    traverse.push(data)
+  })
+  console.log('postorder', traverse)
+})
+
+const hashTable = new HashTable(10, 'double hashing')
 hashTable.set(1, 1)
 hashTable.set(1, 2)
 hashTable.set(1, 10)
 hashTable.set(2, 3)
 hashTable.set(3, 4)
-console.log(hashTable.hashTable, hashTable.collisions)
+console.log(hashTable.table, hashTable.collisions)
